f371bf56e8eb010c47704bee35c8c30074ad1021
Samuel <sumlebulle@hotmail.com>
2019-02-19 11:29:48 +0100

    Added a 404 file

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php /** The template for displaying 404 pages (Not Found)*/ ?>
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+        <div id="content" class="site-content" role="main">
+
+            <header class="page-header">
+                <h1 class="page-title"><?php _e( 'Not Found', 'twentythirteen' ); ?></h1>
+            </header>
+
+            <div class="page-wrapper">
+                <div class="page-content">
+                    <img src="https://media.giphy.com/media/VwoJkTfZAUBSU/giphy.gif" alt="">
+                    <h2><?php _e( 'This is somewhat embarrassing, isn’t it?', 'twentythirteen' ); ?></h2>
+                </div><!-- .page-content -->
+            </div><!-- .page-wrapper -->
+
+        </div><!-- #content -->
+    </div><!-- #primary -->
